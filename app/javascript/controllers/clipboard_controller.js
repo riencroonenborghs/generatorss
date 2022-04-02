f23759f3c2e14d8a67a73adea038a2edae697149
Rien Croonenborghs <rien@croonenborghs.net>
2022-04-03 09:47:02 +1200

    copy url to clipboard

---
+++ app/javascript/controllers/clipboard_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = [ "copyLink", "url", "copiedText" ];
+  
+  copyClicked () {
+    const url = this.urlTarget.value;
+    navigator.clipboard.writeText(url);
+    this.copyLinkTarget.classList.add("d-none");
+    this.copiedTextTarget.classList.remove("d-none");
+
+    const copyLinkTarget = this.copyLinkTarget;
+    const copiedTextTarget = this.copiedTextTarget;
+
+    setTimeout(function() {
+      copyLinkTarget.classList.remove("d-none");
+      copiedTextTarget.classList.add("d-none");
+    }, 1500);
+  }
+}
